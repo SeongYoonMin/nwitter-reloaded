4fdb4f48d268db1b8cf0a0662202b346a2312dcc
SeongYoonMin <namo4862@naver.com>
2024-10-23 22:52:02 +0900

    feat: Default Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,53 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import Account from "./routes/account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "account",
+        element: <Account />,
+      },
+    ],
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+${reset};
+* {
+  box-sizing: border-box;
+}
+body {
+background-color: black;
+color: white;
+}
+`;
+
 function App() {
   return (
     <>
-      <div></div>
+      <GlobalStyles />
+      <RouterProvider router={router} />
     </>
   );
 }
